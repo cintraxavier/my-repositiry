6c08b1b6de17abfa080fcefd7ac3a51ac5b5cbb0
cintraxavier <warbear6@gmail.com>
2021-12-06 19:30:22 -0300

    Que raios para colocar uma coisa nesta joça

--- teste.py
+++ teste.py
@@ -1,2 +1,35 @@
 print(2+3)
+print("ATPC para a hoje 06/12, pontos necessarios, provões 07/12 - fechamento ")
+print("Compensa de falta para 09 a 12/12 entrega de trabalhos")
+print("Saresp 09 e 10/12")
+Sei = "Habilidade de matematica"
+Entrega no plano 10/12 
+aplicação para 13 a 23/12
 
+Escolhe uma habilidade - adaptar para entregue para os outros
+Eletiva - 13 a 16/12 duas ultimas aulas reservadas para prepração
+
+Culminancia 17/12
+6 e 7 ano - 08 as 10h
+
+No plano de aula coloca e verificar. 
+Divulgar aos responsavei
+
+Sala de uso - 12
+dias 20 e 21/12 conselho de classe
+
+confratezinação - 22/12
+
+SED - notas e ausencias - no SED ate 17/12
+Atingirem 75%
+Ninguem reprova. 
+Apresentação da Catia, retonro pregressivo, resolver de projeto para de acordo com livro - livro comunicação não violenta
+Filosifia indiana - hindu, empatia de visao de oriental 
+marshall Rosemberg - Ahimsa - renuncia a violencia, Chopra. Não faço mal, particularidade da atençã. no que faz, 
+4 passos
+observação - olha o que acontece
+sentimento - 
+necessidade - precisa de algo
+pedido - pedido claro
+
+Em rotulo de sentimento - para necessidade de emocional, nao precisa
